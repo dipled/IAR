b70d08811e0dc1263edf5fbad99919e8a226a7e6
pedrovargastannuri@gmail.com <pedrovargastannuri@gmail.com>
2024-03-18 18:44:33 -0300

    atualizei as cores

--- Ants/main.py
+++ Ants/main.py
@@ -46,7 +46,6 @@ def show_grid():
             row_len += 1
        
         y += grid_cell_width   # for every new row we move one "step" downwards
-    pygame.display.update()
     
     y = 0
     for line in dead_grid:
@@ -54,7 +53,7 @@ def show_grid():
         for dead_ant in line:
               
             if dead_ant == 2:
-                createSquare(x, y, (0, 255, 0))
+                createSquare(x, y, (255, 172, 28))
             x += grid_cell_width
 
         y += grid_cell_width
@@ -62,11 +61,16 @@ def show_grid():
 
 if __name__ == '__main__':
     
-    aux = 0
+    done = False
     contf = 0
-    while aux < 5000000:
+
+    while not done:
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    done = True
         random.seed(str(datetime.datetime.now()))
         move(ants, dead_ants, grid, dead_grid, height, width)
-        aux = aux + 1
     # while True:
         show_grid()
+    pygame.quit()
